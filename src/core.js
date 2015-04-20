09350c6c54d411227818146e457c2d434d828094
Giacomo Tagliabue <giacomo.tag@gmail.com>
2015-04-20 11:07:40 -0500

    add pdf generation and style generation

--- src/core.js
+++ src/core.js
@@ -1,11 +1,23 @@
 var Remarkable = require('remarkable');
+var pdf = require('html-pdf');
 
 var md = new Remarkable();
 
 module.exports.markdawn = {
 
-  // generate the document with the following input
-  generate: (text) => {
-    return md.render(text);
+  // generate the pdf document with the following input
+  generate: (text, out, css) => {
+
+    var options = {
+      filename: `./${out}`,
+      format: 'Letter'
+    };
+
+    var html = md.render(text);
+    html = `<!DOCTYPE html><html><body><article class="markdown-body">${html}</article></body><style>${css}</style></html>`
+
+    pdf.create(html, options).toFile(function(err, res) {
+      if (err) return console.log(err);
+    });
   }
 };
--- src/main.js
+++ src/main.js
@@ -6,10 +6,31 @@ var markdawn = require('./core').markdawn;
 // create the program object to parse options
 program
   .usage('[options] <file>')
+  .option('-o, --out <out>', 'name of output file')
+  .option('-s, --style <style.css>', 'name of output file')
   .parse(process.argv);
 
-// parse the input
+// read the input
 var inputFile = program.args[0] || 'input.md';
-var inputText = fs.readFileSync(inputFile, 'utf8');
+var input = fs.readFileSync(inputFile, 'utf8');
 
-console.log(markdawn.generate(inputText));
+// read the css file
+var cssFile = program.style || 'style.css';
+var css = fs.readFileSync(cssFile, 'utf8');
+
+var output = program.out || changeExtension(inputFile, 'pdf');
+
+// call markdawn to generate the pdf
+markdawn.generate(input, output, css);
+
+// utility functions
+
+function changeExtension (fileName, extension){
+  var to = fileName.lastIndexOf(".");
+
+  if(to < 0)
+    to = fileName.length;
+
+  var name = fileName.substr(0, to);
+  return `${name}.${extension}`;
+}
